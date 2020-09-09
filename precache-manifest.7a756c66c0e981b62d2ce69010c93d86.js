2634f833e645772dce50240d4d79cfd439eea16d
Benny <bennyhinnotieno@gmail.com>
2020-09-09 19:02:10 +0300

    Updates

---
+++ precache-manifest.7a756c66c0e981b62d2ce69010c93d86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acba685f3906865825722dc0e5f97901",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "44fcc25168fa7b063938",
+    "url": "/React-Todo-App/static/css/main.3f5f79f3.chunk.css"
+  },
+  {
+    "revision": "e31eb17670f23fd1c5e9",
+    "url": "/React-Todo-App/static/js/2.642fd422.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/React-Todo-App/static/js/2.642fd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fcc25168fa7b063938",
+    "url": "/React-Todo-App/static/js/main.a00dac54.chunk.js"
+  },
+  {
+    "revision": "41829a5898176e7741bd",
+    "url": "/React-Todo-App/static/js/runtime-main.f1a39dbb.js"
+  }
+]);
